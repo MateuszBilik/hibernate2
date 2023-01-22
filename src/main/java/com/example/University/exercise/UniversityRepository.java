ca9004308d23bee1f4276f5f260cf61a8ae89258
mati <1matii1@wp.pl>
2023-01-22 13:55:52 +0100

    more queries

--- src/main/java/com/example/University/exercise/UniversityRepository.java
+++ src/main/java/com/example/University/exercise/UniversityRepository.java
@@ -1,6 +1,18 @@
 package com.example.University.exercise;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface UniversityRepository extends JpaRepository<University, Long> {
+    @Query("select u from University u where u.fee < ?1")
+    List<University> findUniversitiesCheaperThan(Long fee);
+    @Query("select u from University u where u.fee is null")
+    List<University> findUniversityWithoutFee();
+
+    Optional<University> findByStreetAndCityAndPostalCodeAndCountry(String street, String city, String postalCode, String country);
+
+
 }
